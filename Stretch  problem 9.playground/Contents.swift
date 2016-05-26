e5aa88ded2f1c6ffeddfddbffcada606c150d670
Eva <Bresciano>
2016-05-26 10:01:31 -0600

    finished

--- Stretch  problem 9.playground/Contents.swift
+++ Stretch  problem 9.playground/Contents.swift
@@ -6,25 +6,24 @@ var str = "Hello, playground"
 
 var intArray = [0,14,70,93,6,32,21,99,2,7]
 
+var statement: String = ""
+var comparableNumber: Int = 0
 func loopfunc(numbers: [Int]) {
     for number in numbers {
-        if number < 50 {
-            print ("high")
-        } else {
-                print("low")
-            }
-        if number % 13 == 0 {
-            print("didnt learn this one in school")
-            
-        } else {
-            print("thats easy")
-        }
-       
+        number > 50 ? (statement += "High, ") : (statement += "Low, ")
+        number % 13 == 0 ? (statement += "Easy, ") : (statement += "Did not learn this, ")
+        number > comparableNumber ? (statement += "getting bigger, ") : (statement += "stopped growing, ")
+
+        print(statement)
+        statement == ""
         
-    }
-    
-    
-    
+     }
     }
 
 loopfunc(intArray)
+
+var value: String? = "hey i have a value"
+var nilString: String? = nil
+
+print(value ?? "")
+print(nilString ?? "")
