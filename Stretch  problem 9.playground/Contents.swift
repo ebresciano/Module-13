7000ef06854545423cbc7464e025d4ebc772b756
Eva <Bresciano>
2016-05-26 09:33:01 -0600

    stretch start

---
+++ Stretch  problem 9.playground/Contents.swift
@@ -0,0 +1,30 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var intArray: [Int] = [0,14,70,93,6,32,21,99,2,7]
+
+func loopfunc(intArray: [Int]) {
+    for i in intArray {
+        if i > 50 {
+            print ("low")
+        } else {
+                print("high")
+            }
+        if i % 13 == 0 {
+            print("didnt learn this one in school")
+            
+        } else {
+            print("thats easy")
+        }
+        if i == Int +1
+        
+    }
+    
+    
+    
+    }
+
+print(intArray)
